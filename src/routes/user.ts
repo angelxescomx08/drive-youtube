7bc0c0a3ee6d82fc630b50d1349f22eb48a7f673
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2024-06-02 12:52:37 -0600

    insertar usuario

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,4 +1,7 @@
 import { Router } from "express";
+import { db } from "../db/db";
+import { user } from "../db/schema";
+import bcrypt from "bcryptjs";
 
 const userRouter = Router();
 
@@ -8,10 +11,33 @@ userRouter.get("/", (req, res) => {
   });
 });
 
-userRouter.post("/", (req, res) => {
-  res.json({
-    message: "POST",
-  });
+userRouter.post("/", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const hashPassword = bcrypt.hashSync(password);
+
+    const result = await db
+      .insert(user)
+      .values({
+        email,
+        id_user: crypto.randomUUID(),
+        password: hashPassword,
+      })
+      .returning({
+        email: user.email,
+        id_user: user.id_user,
+      });
+
+    res.json({
+      message: "User created",
+      user: result.at(0),
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Something wrong happen",
+    });
+  }
 });
 
 userRouter.put("/", (req, res) => {
