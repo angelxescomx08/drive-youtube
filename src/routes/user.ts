66e8642df2a1da3311c89fad01f9ddde1d40c3f0
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2024-07-21 16:24:00 -0600

    middlewares

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -2,11 +2,12 @@ import { Router } from "express";
 import { createUserController } from "../controllers/user/create-user";
 import { loginController } from "../controllers/user/login";
 import { getUsers } from "../controllers/user/list-users";
+import { validateToken } from "../middlewares/auth/validate-token";
 
 
 const userRouter = Router();
 
-userRouter.get("/", getUsers);
+userRouter.get("/", [validateToken], getUsers);
 
 userRouter.post("/", createUserController);
 
