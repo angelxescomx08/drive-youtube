f666da43fc1b5e83bca35c7b8deb2f8e0f290501
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2024-12-28 12:38:28 -0600

    actualizar files

---
+++ src/controllers/file/update-file-controller.ts
@@ -0,0 +1,67 @@
+import { LibsqlError } from "@libsql/client";
+import { Request, Response } from "express";
+import { db } from "../../db/db";
+import { eq } from "drizzle-orm";
+import { file } from "../../db/schema";
+import { updateFileSchema } from "../../types/file";
+
+export const updateFileController = async (req: Request, res: Response) => {
+  try {
+
+    const { id_file } = req.params;
+    const { id_folder, file_name } = req.body;
+
+    const validateFields = updateFileSchema.safeParse({
+      id_file,
+      id_folder, 
+      file_name
+    })
+
+    if (!validateFields.success) {
+      return res.status(400).json({
+        message: "Not valid data",
+        error: validateFields.error
+      })
+    }
+
+    const fileToUpdate = await db.query.file.findFirst({
+      where: eq(file.id_file, id_file)
+    })
+
+    if (!fileToUpdate) {
+      return res.status(400).json({
+        message: "There is not any file with that id",
+        error: "There is not any file with that id"
+      })
+    }
+
+    const updatedFile = await db.update(file).set({
+      id_folder, 
+      file_name
+    }).where(eq(file.id_file, id_file))
+      .returning({
+        id_file: file.id_file,
+        id_folder: file.id_folder,
+        file_name: file.file_name,
+        url: file.url,
+        aws_key: file.aws_key,
+      })
+
+    res.status(200).json({
+      message: "Query successful",
+      file: updatedFile.at(0),
+    })
+
+  } catch (error) {
+    if (error instanceof LibsqlError) {
+      return res.status(500).json({
+        message: error.message,
+        error
+      });
+    }
+    res.status(500).json({
+      message: "Something wrong happen unfortunately",
+      error
+    });
+  }
+}
--- src/types/file.ts
+++ src/types/file.ts
@@ -18,9 +18,19 @@ export const getFileSchema = fileSchema.pick({
   id_file: true
 })
 
+export const updateFileSchema = fileSchema.extend({
+  id_folder: fileSchema.shape.id_folder.optional(),
+  file_name: fileSchema.shape.file_name.optional(),
+}).pick({
+  id_file: true,
+  id_folder: true,
+  file_name: true,
+})
+
 export type typeFile = z.infer<typeof fileSchema>
 export type typeCreateFile = z.infer<typeof createFileSchema>
 export type typeGetFile = z.infer<typeof getFileSchema>
+export type typeUpdateFile = z.infer<typeof updateFileSchema>
 
 export type FileS3 = {
   fieldname: string,
