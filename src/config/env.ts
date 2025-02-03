36327fa949257b93ad31d12400d9598ae520d950
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2025-02-03 17:16:45 -0600

    changes

--- src/config/env.ts
+++ src/config/env.ts
@@ -5,7 +5,7 @@ export const envs = {
   SECRET_PASSWORD_KEY: env.get('SECRET_PASSWORD_KEY').required().asString(),
   DATABASE_URL: env.get('DATABASE_URL').required().asString(),
   TURSO_AUTH_TOKEN: env.get('TURSO_AUTH_TOKEN').required().asString(),
-  MAIL: env.get('MAIL').required().asEmailString(),
+  //MAIL: env.get('MAIL').required().asEmailString(),
   PORT: env.get('PORT').required().asIntPositive(),
 
   AWS_ACCESS_KEY_ID: env.get("AWS_ACCESS_KEY_ID").required().asString(),
